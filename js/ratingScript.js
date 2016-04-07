1729e1fb56bda6529cd7bcec5351b7e521a2cdf5
Shaif821 <Shaif.bhaggoe@gmail.com>
2016-04-07 20:12:45 +0200

    definitieve codes

--- js/ratingScript.js
+++ js/ratingScript.js
@@ -1,4 +1,12 @@
-var aantalGev
+//                  Rond af naar boven
+var aantalWaarderingen = Math.ceil(5*Math.random());
+var totaalKliks = aantalWaarderingen*Math.random()*5;
+var uitvoerStuks = document.getElementById('stuks');
+var uitvoerRating = document.getElementById('rating');
+var geenSterren = document.querySelectorAll('.geen');
+
+
+
 function gevuldeSter(sterNummer, procent) {
     var naam = 'ster' + sterNummer;
     //Zorgt ervoor dat het maar 1 ster bereikt:
@@ -17,4 +25,23 @@ function  gevulDeSterren(rating) {
 }
 
 
-gevulDeSterren(3,2);
+// willkeurig generen en uitvoeren in DOM
+function uitvoeren() {
+uitvoerStuks.innerHTML = aantalWaarderingen
+//                            Rond af          Zorgt voor kleine getallen
+uitvoerRating.innerHTML = Math.ceil(10*totaalKliks/aantalWaarderingen)/10;
+gevulDeSterren(Math.ceil(10*totaalKliks/aantalWaarderingen)/10)
+}
+
+uitvoeren();
+
+
+function uitvoerKlik() {
+    aantalWaarderingen ++;
+    var sternummertje = this.getAttribute('data-role');
+    totaalKliks += parseInt(sternummertje);
+    uitvoeren();
+}
+for(var i=0; i<geenSterren.length; i++) {
+    geenSterren[i].addEventListener('click', uitvoerKlik);
+}
